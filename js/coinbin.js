b5ac247183890a642985499436ab3bee0110b840
anoxy <anoxy@pop-os.localdomain>
2022-05-27 12:56:23 +0200

    remove other coins from settings page

--- js/coinbin.js
+++ js/coinbin.js
@@ -3694,7 +3694,7 @@ observer.observe(target, config);
 			//configureBroadcast();
 			configureGetUnspentTx();
 
-			$("#statusSettings").addClass("alert-success").removeClass("hidden").html("<span class=\"glyphicon glyphicon-ok\"></span> Settings updates successfully").fadeOut().fadeIn();	
+			$("#statusSettings").addClass("alert-success").removeClass("hidden").html("<span class=\"glyphicon glyphicon-ok\"></span> Settings updated successfully").fadeOut().fadeIn();	
 		} else {
 			$("#statusSettings").addClass("alert-danger").removeClass("hidden").html("There is an error with one or more of your settings");	
 		}
@@ -3723,11 +3723,21 @@ observer.observe(target, config);
 		}
 
 		// deal with the reset
+		/*
 		$("#coinjs_pub").val(o[0]);
 		$("#coinjs_priv").val(o[1]);
 		$("#coinjs_multisig").val(o[2]);
 		$("#coinjs_hdpub").val(o[3]);
 		$("#coinjs_hdprv").val(o[4]);
+		*/
+
+		$("#coinjs_pub").val('0x'+(coinjs.pub).toString(16));
+		$("#coinjs_priv").val('0x'+(coinjs.priv).toString(16));
+		$("#coinjs_multisig").val('0x'+(coinjs.multisig).toString(16));
+
+		$("#coinjs_hdpub").val('0x'+(coinjs.hdkey.pub).toString(16));
+		$("#coinjs_hdprv").val('0x'+(coinjs.hdkey.prv).toString(16));	
+
 
 		$("#coinjs_extratimefield").val(o[7]);
 		$("#coinjs_symbol").val(o[8]);
